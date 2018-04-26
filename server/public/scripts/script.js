140f6a176a62e591d90090719a6bd9741a8d2bc1
llenk <ellenkeal@gmail.com>
2018-04-26 09:25:10 -0500

    bling http request added

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,7 +1,17 @@
 var app = angular.module('FoodApp', []);
 
-app.controller('FoodController', function() {
+app.controller('FoodController', function($http) {
     console.log('FoodController loaded');
 
     var self = this;
-})
+
+    $http({
+        method: 'GET',
+        url: '/food'
+    }).then(function successCallback(response) {
+        console.log(response.data);
+    }, function errorCallback(response) {
+        self.foodArray = response.statusText;
+    });
+});
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static('server/public'));
 
+let foodArray = [
+    {name: 'Noodles', 
+        deliciousness_rating: 4,
+        is_hot: false}
+];
+
+app.get('/food', function(req, res) {
+    console.log('eyy');
+    res.send(foodArray);
+});
+
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
 });
